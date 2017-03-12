b7310818175b4bd43d009361f9f1081a20dd24f0
Jorge DE La Cruz <jorge.cruz.arcia@rwth-aachen.de>
2017-03-12 23:17:53 +0100

    ReConBot Class for Matlab improved

--- 02_MATLAB_Code/traj_pub.m
+++
@@ -1,7 +0,0 @@
-function y=traj_pub(trajectory)
-publisher = rospublisher('/reconbot_trajectory','control_msgs::FollowJointTrajectoryGoal');
-pause(2);
-send(publisher,trajectory);
-pause(2);
-end
-
