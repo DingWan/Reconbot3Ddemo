11a94dd924d7e976115cef3ec852122b23db5b21
ding <ding@igm.rwth-aachen.de>
2017-05-29 20:46:53 +0200

    FK 3T1R debugging

--- 02_MATLAB_Code/Reconbot3Ddemo/Modes/FK.m
+++ 02_MATLAB_Code/Reconbot3Ddemo/Modes/FK.m
@@ -1,10 +1,10 @@
 clf
-l1 = 230.1390;
-l2 = 147.7;
+l1 = 0.2301390;
+l2 = 0.1477;
 deg = pi/180;
 PosOri = {0 0 0.208879343162506 0 [] [], pi/2 -pi/2};
 %% --3T1R--
-q11q12q21q22 = [0*pi/3, pi/4, -0*pi/6, pi/3];
+q11q12q21q22 = [0.1*pi/3, pi/4, 0*pi/3, pi/4];
 obj3T1R = RCB3T1R(PosOri, q11q12q21q22, l1, l2);
 [p, ABC, q1q2] = obj3T1R.RCB_3T1R_FK;
 
