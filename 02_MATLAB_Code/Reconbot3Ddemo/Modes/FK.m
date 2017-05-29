0c2bf7b7d8a0a94324cbbc310729f29b55e723f9
ding <ding@igm.rwth-aachen.de>
2017-05-29 17:56:06 +0200

    FK Mode 3T1R

--- 02_MATLAB_Code/Reconbot3Ddemo/Modes/FK.m
+++ 02_MATLAB_Code/Reconbot3Ddemo/Modes/FK.m
@@ -1,16 +1,16 @@
 clf
-l1 = 230.0692;
-l2 = 146.25;
+l1 = 230.1390;
+l2 = 147.7;
 deg = pi/180;
-PosOri = {0 0 253.3124 0 [] [], pi/2 -pi/2};
+PosOri = {0 0 0.208879343162506 0 [] [], pi/2 -pi/2};
 %% --3T1R--
-% q11q12q21q22 = [pi/3, pi/3, -pi/6, pi/3];
-% obj3T1R = RCB3T1R(PosOri, q11q12q21q22, l1, l2);
-% [p, ABC, q1q2] = obj3T1R.RCB_3T1R_FK;
+q11q12q21q22 = [0*pi/3, pi/4, -0*pi/6, pi/3];
+obj3T1R = RCB3T1R(PosOri, q11q12q21q22, l1, l2);
+[p, ABC, q1q2] = obj3T1R.RCB_3T1R_FK;
 
 %% --2T2R--
-q11q12q14q23 = [pi/3, 0.5*pi/4,1*pi/6, 1*pi/3];
-obj2T2Rsixbar = RCB2T2Rsixbar(PosOri, q11q12q14q23 , l1, l2);
-[p, ABC, q1q2] = obj2T2Rsixbar.RCB_2T2Rsixbar_FK;
-grid on
-axis equal
+% q11q12q14q23 = [pi/3, 0.5*pi/4,1*pi/6, 1*pi/3];
+% obj2T2Rsixbar = RCB2T2Rsixbar(PosOri, q11q12q14q23 , l1, l2);
+% [p, ABC, q1q2] = obj2T2Rsixbar.RCB_2T2Rsixbar_FK;
+% grid on
+% axis equal
