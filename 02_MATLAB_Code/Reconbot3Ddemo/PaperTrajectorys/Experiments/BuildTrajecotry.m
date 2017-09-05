04840d2bcab4ba23339c388f4236dd5ef90d54ff
ding <ding@igm.rwth-aachen.de>
2017-09-05 17:15:42 +0200

    Experiments in Folder

--- 02_MATLAB_Code/Reconbot3Ddemo/PaperTrajectorys/Experiments/BuildTrajecotry.m
+++ 02_MATLAB_Code/Reconbot3Ddemo/PaperTrajectorys/Experiments/BuildTrajecotry.m
@@ -13,7 +13,7 @@ robot = ReConBot;
 robot.topic = '/reconbot_trajectory';
 robot.jointNames = {'joint_4','joint_5','joint_3','joint_1','joint_2','joint_6'};
 robot.pos = data;
-robot.timeFromStart = 5;
+robot.timeFromStart = 4;
 robot.trajPoints = robot.transform();
 %robot.tj = robot.buildTrajectory();
 
